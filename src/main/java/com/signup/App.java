1c5ef80434723a54131679febd01215121b63a9d
29SahanaV <sahanagowda90089@gmail.com>
2022-08-18 17:03:58 +0530

    signup project

---
+++ src/main/java/com/signup/App.java
@@ -0,0 +1,17 @@
+package com.signup;
+
+/**
+ * create a signup page by using REGEX and EXCEPTION HANDLING
+ *
+ */
+import java.util.Scanner;
+import static java.lang.System.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Scanner sa=new Scanner(System.in);    //scanner object
+       signupvalidations
+       
+    }
+}
